ba59cbabb876d98acca05001d5dfa03c06c4bc54
micadevcon <micadevcon@mail.ru>
2022-12-03 18:23:24 +0400

    Финал
    финальное решение

---
+++ Vowel Count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Vowel_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            
+            Console.WriteLine(Vowel_Count(str));
+        }
+        static public int Vowel_Count(string str)
+        {
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o','u' };
+            int coincidences = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int b = 0; b < vowels.Length; b++)
+                {
+                    if (str[i]==vowels[b])
+                    {
+                        coincidences++;
+                    }
+                }
+            }
+            return coincidences;
+        }
+    }
+}
